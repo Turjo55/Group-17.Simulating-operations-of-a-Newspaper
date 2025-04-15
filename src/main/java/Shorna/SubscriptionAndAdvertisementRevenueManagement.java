b70f60033e55ce4cad60724b1a9cec6f64bcc1f8
Morium Akter Shorna <morium870@gmail.com>
2025-04-15 12:21:07 +0600

    Merge remote-tracking branch 'origin/main'

--- src/main/java/Shorna/SubscriptionAndAdvertisementRevenueManagement.java
+++ src/main/java/Shorna/SubscriptionAndAdvertisementRevenueManagement.java
@@ -1,14 +1,7 @@
 package Shorna;
 
-import javafx.scene.control.TextField;
-
 public class SubscriptionAndAdvertisementRevenueManagement
 {
-    @javafx.fxml.FXML
-    private TextField userNameTextField;
-    @javafx.fxml.FXML
-    private TextField passwordTextFVield;
-
     @javafx.fxml.FXML
     public void initialize() {
     }}
