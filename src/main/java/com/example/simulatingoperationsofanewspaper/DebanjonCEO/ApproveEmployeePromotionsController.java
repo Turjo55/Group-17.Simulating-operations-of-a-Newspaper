e9dc25beab93c39cdcd45863d9005e6cc35b39e5
Debanjon Paul Dip <dip4paul@gmail.com>
2025-04-18 23:04:19 +0600

    18th commit

--- src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonCEO/ApproveEmployeePromotionsController.java
+++ src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonCEO/ApproveEmployeePromotionsController.java
@@ -1,4 +1,34 @@
 package com.example.simulatingoperationsofanewspaper.DebanjonCEO;
 
-public class ApproveEmployeePromotionsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+
+    public class ApproveEmployeePromotionsController {
+
+        @FXML
+        private VBox promotionCriteriaVBox;
+
+        @FXML
+        private ComboBox<?> selectEmployeeComboBox;
+
+        @FXML
+        void approvePromotionButton(ActionEvent event) {
+
+        }
+
+        @FXML
+        void backButton(ActionEvent event) {
+
+        }
+
+        @FXML
+        void rejectButton(ActionEvent event) {
+
+        }
+
+
+    }
+
 }
--- src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonCEO/SendCompanyAnnouncements.java
+++ src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonCEO/SendCompanyAnnouncements.java
@@ -45,5 +45,6 @@ public class SendCompanyAnnouncements {
         this.notification = notification;
 
 
+
     }
 }
