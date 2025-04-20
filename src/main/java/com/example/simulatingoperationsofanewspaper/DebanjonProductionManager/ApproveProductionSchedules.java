0cedd8386df9a9141d16709daa28aa00ebb9ff3f
Debanjon Paul Dip <dip4paul@gmail.com>
2025-04-21 02:12:46 +0600

    21 commit

--- src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonProductionManager/ApproveProductionSchedules.java
+++ src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonProductionManager/ApproveProductionSchedules.java
@@ -8,11 +8,33 @@ public class ApproveProductionSchedules {
     private String task;
     private String status;
 
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public ApproveProductionSchedules(String scheduleId, String productionDate, String shift, String task, String status) {
         this.scheduleId = scheduleId;
         this.productionDate = productionDate;
         this.shift = shift;
         this.task = task;
         this.status = status;
+
+
     }
 }
