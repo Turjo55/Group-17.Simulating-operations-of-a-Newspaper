cd5b349afea52acdad077bbdf1c5eab45f64508e
Turjo55 <tanvirulturjo@gmail.com>
2025-04-20 19:43:46 +0600

    TanvirulADMIN section MonitoringWebsiteTrafficController goal TableView code

--- src/main/java/com/example/simulatingoperationsofanewspaper/HelloApplication.java
+++ src/main/java/com/example/simulatingoperationsofanewspaper/HelloApplication.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        String fxmlPath = "/com/example/simulatingoperationsofanewspaper/TanvirulADMIN/ManagingUserAccounts.fxml";
+        String fxmlPath = "/com/example/simulatingoperationsofanewspaper/TanvirulADMIN/MonitoringWebsiteTraffic.fxml";
         System.out.println("Loading FXML from: " + fxmlPath);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
         Parent root = fxmlLoader.load();
