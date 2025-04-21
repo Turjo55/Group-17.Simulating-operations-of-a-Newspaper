0319693d896f7585ecd1e7f304a3541d3d8a0209
Debanjon Paul Dip <dip4paul@gmail.com>
2025-04-21 14:54:05 +0600

    Recovered code after reset

--- src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonProductionManager/ResolveComplaints.java
+++ src/main/java/com/example/simulatingoperationsofanewspaper/DebanjonProductionManager/ResolveComplaints.java
@@ -1,4 +1,13 @@
 package com.example.simulatingoperationsofanewspaper.DebanjonProductionManager;
 
 public class ResolveComplaints {
+    protected Str name:
+    private   Str  type:
+
+    public ResolveComplaints(Str name, Str type) {
+        this.name = name;
+        this.type = type;
+
+
+    }
 }
